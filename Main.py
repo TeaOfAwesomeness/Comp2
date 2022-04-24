34a19fa0a814f16a3c3b54fd1a049501ad65a4fd
TeaOfAwesomeness <tom@priestfamily.net>
2022-04-24 22:20:27 +0100

    Training data function works.

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ import copy
 class Main:
     # Global variables
     network = None
+    ls_button_matrix = list([])
 
     def __init__(self):
         root = tk.Tk()
@@ -22,22 +23,45 @@ class Main:
 
     def create_gui(self, tk, frame):
         # Command function for btn_gen
-        def generate_network():
+        def generate_network(self):
             # Return text values
             inodes = txt_in.get("1.0", "end-1c")
             onodes = txt_out.get("1.0", "end-1c")
             # Send numeric values to network
             if inodes.isnumeric() and onodes.isnumeric():
                 self.network = Network(int(inodes), int(onodes))
+                for i in range(0, int(onodes)):
+                    y_coord = 4 + self.network.ls_onodes[i].get_y_coord()  #Row in matrix offset by GUI elements
+                    x_coord = self.network.ls_onodes[i].get_x_coord()
+                    btn_temp = tk.Button(frame)
+                    if i == 0:
+                        self.ls_button_matrix = list([btn_temp])
+                    else:
+                        self.ls_button_matrix.extend([btn_temp])
+                    self.ls_button_matrix[i].grid(row=y_coord, column=x_coord, sticky=tk.EW)
 
         # Command function of btn_train
         def train_network():
+            # Set variables
             lr = txt_lr.get("1.0", "end-1c")
-            if lr.isnumeric():
-                input_data = self.read(txt_train.get("1.0", "end-1c"))
-                if len(input_data) > 1:
-                    self.network.train(input_data, len(input_data), lr)
-                    print("train test")
+            input_data = self.read(txt_train.get("1.0", "end-1c"))
+
+            # Get BMUs
+            if len(input_data) > 1:
+                bmu_list = self.network.train(input_data, len(input_data), float(lr))
+                # print("train test")
+
+                # Blank buttons
+                for i in range(0, len(self.ls_button_matrix)):
+                    self.ls_button_matrix[i].config(text="")  # Reset button text for new trained BMUs
+                # Update buttons with dataset names
+                for i in range(0, len(bmu_list)):
+                    print("Node: ", bmu_list[i].node_id)
+                    temp_string = "BMU: "  # New string for button text
+                    for j in range(0, len(bmu_list[i].ls_dataset_names)):
+                        temp_name = str(bmu_list[i].ls_dataset_names[j])  # Get newest dataset name linked to this BMU
+                        temp_string = temp_string + temp_name + ", "  # Add new dataset name to the rest
+                    self.ls_button_matrix[bmu_list[i].node_id].config(text=copy.deepcopy(temp_string))  # Set new text
 
         # Command function of btn_test
         def test_network():
@@ -53,7 +77,7 @@ class Main:
         lbl_net = tk.Label(frame, text="Network").grid(row=3, column=1, columnspan=2)
 
         # All function button widgets
-        btn_gen = tk.Button(frame, command=generate_network, width=2).grid(row=0, column=5)
+        btn_gen = tk.Button(frame, command=lambda: generate_network(self), width=2).grid(row=0, column=5)
         btn_train = tk.Button(frame, command=train_network, width=2).grid(row=1, column=4)
         btn_test = tk.Button(frame, command=test_network, width=2).grid(row=2, column=2)
 
@@ -100,10 +124,10 @@ class Main:
 class Network:
     num_inodes = 0
     num_onodes = 0
-    ls_inodes = list((0, 1))
-    ls_onodes = None
+    ls_inodes = list([])
+    ls_onodes = list((0, 1))
     arr_onodes = None
-    radius_const = 0
+    map_radius = 0
     map_width = 0
     bmu_list = None
 
@@ -111,7 +135,7 @@ class Network:
         self.num_inodes = num_inodes
         self.num_onodes = num_onodes
         self.map_width = round(math.sqrt(num_onodes))  # Calculate map width
-        map_radius = self.map_width / 2  # Initial learning radius
+        self.map_radius = self.map_width / 2  # Initial learning radius
         node_num = 0  # Setup node id
 
         # Make array of ONodes of size map width by map width
@@ -121,23 +145,29 @@ class Network:
                 onode = ONode(node_num, i, j, num_inodes)  # New node
                 onode.init_weights(num_inodes)  # Randomise weights
                 if i == 0 and j == 0:
-                    ls_onodes = list([copy.deepcopy(onode)])
+                    self.ls_onodes = list([copy.deepcopy(onode)])
                 else:
-                    ls_onodes.append([copy.deepcopy(onode)])
+                    self.ls_onodes.extend([copy.deepcopy(onode)])
                 node_num += 1
 
         # Create list of input nodes
         for i in range(0, num_inodes):
             inode = INode(i)
             if i == 0:
-                self.ls_inodes = list([inode])
+                self.ls_inodes = list([copy.deepcopy(inode)])
             else:
-                self.ls_inodes.append([copy.deepcopy(inode)])
+                self.ls_inodes.extend([copy.deepcopy(inode)])
         print("hello world")
 
     # Train network on 'inputs' data
     def train(self, inputs, time_const, learn_rate):
         time_current = 0
+        bmu_list = None
+
+        # Re-set ONodes
+        for i in range(0, self.num_onodes):
+            self.ls_onodes[i].init_weights(self.num_inodes)  # Re-set weights
+            self.ls_onodes[i].ls_dataset_names = None  # Re-set list of BMUs
 
         # Loop through all training data
         for i in range(0, len(inputs)):
@@ -145,7 +175,7 @@ class Network:
 
             # Load data into input nodes
             current_data_list = inputs[i]
-            dataset_name = current_data_list[0]
+            dataset_name = str(current_data_list[0])
             for j in range(0, self.num_inodes):
                 x=j+1
                 if current_data_list[x].isnumeric:
@@ -156,33 +186,41 @@ class Network:
             bmu = self.push(dataset_name)
             # Add BMU to list
             if bmu_list is None:
-                bmu_list = [bmu]
+                bmu_list = list([copy.deepcopy(bmu)])
             else:
-                bmu_list.append(bmu)
+                bmu_list.extend([copy.deepcopy(bmu)])
 
             # Update training constants
             new_learn_rate = learn_rate * math.exp(-(time_current/time_const))  # Fraction of previous w/ respect to time
-            radius_current = self.radius_const * math.exp(-(time_current/time_const))  # Fraction of previous w/ respect to time
+            radius_current = self.map_radius * math.exp(-(time_current/time_const))  # Fraction of previous w/ respect to time
 
             # Update weights
-            for x in range(0, self.map_width):
-                for y in range(0, self.map_width):
-                    bmu_dist = math.sqrt(pow((x - bmu.x_coord), 2) + pow((y - bmu.ycoord), 2))  # Calculate distance to bmu using pythagoras
-                    if bmu_dist <= radius_current:
-                        self.arr_onodes([x], [y]).modify_weights(radius_current, new_learn_rate, bmu_dist)
+            for i in range(0, self.num_onodes):
+                # Set x and y coords of current node
+                x = self.ls_onodes[i].get_x_coord()
+                y = self.ls_onodes[i].get_y_coord()
+
+                # Update weight if node is in range of BMU
+                bmu_dist = math.sqrt(pow((x - bmu.x_coord), 2) + pow((y - bmu.y_coord), 2))  # Calculate distance to bmu using pythagoras
+                if bmu_dist <= radius_current:
+                    self.ls_onodes[i].modify_weights(radius_current, new_learn_rate, bmu_dist)
         return bmu_list
 
     # Using one set of inputs, iterate once over the network
     def push(self, dataset_name):
+        # Set initial BMU
+        current_best = self.ls_onodes[0]
+
         # Push data through network to all output nodes
-        for x in range(0, self.map_width):
-            for y in range(0, self.map_width):
-                for j in range(0, self.num_inodes):
-                    self.arr_onodes([x], [y]).set_input(j, self.ls_inodes(j).get_val())  # Update input val from INode
-                output = self.arr_onodes([x], [y]).get_output()  # Calculate result of weighted inputs
-                if output << current_best or current_best is None:
-                    current_best = output  # Update BMU
-        self.arr_onodes([current_best.x_coord], [current_best.y_coord]).add_bmu(dataset_name)
+        for i in range(0, self.num_onodes):
+            for j in range(0, self.num_inodes):
+                self.ls_onodes[i].set_input(j, self.ls_inodes[j].get_val())  # Update input val from INode
+            output = self.ls_onodes[i].get_output()  # Calculate result of weighted inputs
+            if output < current_best.node_out_current:
+                current_best = self.ls_onodes[i]  # Update BMU
+
+        # Update BMU and return it
+        self.ls_onodes[current_best.node_id].add_bmu(dataset_name)
         return current_best
 
 
@@ -209,24 +247,35 @@ class INode:
 # Output node for the network
 class ONode:
     # Define a 2D array for storing input values and their associated weights
-    inputs = np.array([[], []])
+    inputs = np.array([[0], [0]])
     # Coordinates in hypothetical matrix
     x_coord = 0
     y_coord = 0
-    list_dataset_names = None
+    node_id = 0
+    ls_dataset_names = None
+    node_out_current = 0
 
     def __init__(self, node_num, node_x, node_y, number_inputs):
-        self.id = node_num
+        self.node_id = node_num
         self.x_coord = node_x
         self.y_coord = node_y
         self.init_weights(number_inputs)  # Randomise initial weights
+
+    def get_x_coord(self):
+        return self.x_coord
+
+    def get_y_coord(self):
+        return self.y_coord
 
     # Initialise random weights for input connections
     def init_weights(self, number_inputs):
         for i in range(0, number_inputs):
             random_weight = rand.randrange(1, 100)
-            temp_array = np.array([[0], [random_weight]])  # First is stand in for input value, second is random weight value
-            self.inputs = np.append(self.inputs, temp_array, axis=1)  # Nb: python rows and columns are inverted
+            if i == 0:
+                self.inputs = np.array([[i, random_weight]])
+            else:
+                temp_array = np.array([[i, random_weight]])  # First is stand in for input value, second is random weight value
+                self.inputs = np.vstack([self.inputs, temp_array])  # Nb: python rows and columns are inverted
 
     # Sets new input values for the node
     def set_input(self, input_number, input_value):
@@ -239,21 +288,24 @@ class ONode:
         for i in range(0, self.inputs.shape[0]):
             result = result + math.pow((self.inputs[i, 0] - self.inputs[i, 1]), 2) # Summates all squared differences between input value and weight
         result = math.sqrt(result)
+
+        self.node_out_current = result  # Keep record of current output
         return result
 
     # Train weights according to new BMU
     def modify_weights(self, radius_current, learning_rate, bmu_dist):
-        dist_effect = math.exp(-(math.pow(bmu_dist, 2)/(2*radius_current)))
+        dist_effect = math.exp((math.pow(bmu_dist, 2)/(2*radius_current)) * float(-1))
         for i in range(self.inputs.shape[0]):
             temp_val_desired = self.inputs[i, 0]
             temp_weight = self.inputs[i, 1]
             self.inputs[i, 1] = temp_weight + (dist_effect*learning_rate*(temp_val_desired - temp_weight))
+            # print("Old Weight: ", temp_weight, " New Weight: ", self.inputs[i, 1])
 
     def add_bmu(self, dataset_name):
-        if self.list_dataset_names is None:
-            self.list_dataset_names = list(dataset_name)
+        if self.ls_dataset_names is None:
+            self.ls_dataset_names = list([dataset_name])
         else:
-            self.list_dataset_names.append(dataset_name)
+            self.ls_dataset_names.append([dataset_name])
 
 
 if __name__ == "__main__":
